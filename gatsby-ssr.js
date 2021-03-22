7d58f09f5f3b62a02962269d6900ebecb05dc83c
madhumathi0213 <madhumathi@pentafox.in>
2021-03-22 10:59:06 +0530

    error fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+// Requiring function causes error during builds
+// as the code tries to reference window
+const module = require("module") // Error
+// Wrap the require in check for window
+if (typeof window !== `undefined`) {
+  const module = require("module")
+}
