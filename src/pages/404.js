13bf7e1b4165a61015e31b08313caf6e88738802
madhumathi0213 <madhumathi@pentafox.in>
2021-03-22 10:53:38 +0530

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <>
+        <div>
+            <p>404 Error found</p>
+        </div>
+        </>
+    )
+}
+export default ErrorPage;
